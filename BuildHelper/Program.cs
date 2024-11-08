b5cd5ac32ea579a3008700f22c7e8a5a8c905743
Samma - PixelStudio <81291107+Samma2009@users.noreply.github.com>
2024-11-08 20:27:35 +0100

    fixes

--- BuildHelper/Program.cs
+++ BuildHelper/Program.cs
@@ -10,39 +10,40 @@ namespace NativeOS
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string> buildfile = new();
+            Dictionary<string, string> buildfile = new()
+            {
+                {"CORLIB","Corlib\\Corlib.shproj"},
+                {"KERNEL","Kernel\\Kernel.shproj"},
+                {"OS","OS\\(GENERIC OS NAME).csproj"},
+                {"RAMDISK","Ramdisk"},
+                {"VM","VMWare"},
+            };
             foreach (var item in File.ReadAllText(args[0]).Split(Environment.NewLine))
             {
                 var s = item.Split("=");
-                buildfile.Add(s[0], s[1]);
+                buildfile[s[0]] = s[1];
             }
-
-            var Dirname = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) +
-                @"\" +
-                Path.GetRandomFileName();
-            Directory.CreateDirectory(Dirname);
-
-            var info = Assembly.GetExecutingAssembly().GetName();
-            var name = info.Name;
-            using var stream = Assembly
-                .GetExecutingAssembly()
-                .GetManifestResourceStream($"{name}.BaseCode.zip")!;
-
-            ZipFile.ExtractToDirectory(stream, Dirname);
+            var Dirname = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) +@"\CompilerData";
 
             var configPath = Path.GetDirectoryName(Path.GetFullPath(args[0]));
             var corlibPath = Path.GetFullPath(configPath + @"\" + Path.GetDirectoryName(buildfile["CORLIB"]));
             var kernelPath = Path.GetFullPath(configPath + @"\" + Path.GetDirectoryName(buildfile["KERNEL"]));
             var osPath = Path.GetFullPath(configPath + @"\" + Path.GetDirectoryName(buildfile["OS"]));
+            var rdisk = Path.GetFullPath(configPath + @"\" + Path.GetDirectoryName(buildfile["RAMDISK"]));
             CopyDirectory(corlibPath, Dirname + @"\" + "Corlib");
             CopyDirectory(kernelPath, Dirname + @"\" + "Kernel");
+            CopyDirectory(Dirname + @"\" + "MOOSDATA", Dirname + @"\" + "MOOS");
             CopyDirectory(osPath, Dirname + @"\" + "MOOS",".csproj");
+            CopyDirectory(rdisk, Dirname + @"\" + "Ramdisk");
 
-            var a = Environment.CurrentDirectory;
             Environment.CurrentDirectory = Dirname;
             Process.Start("cmd", "/c dotnet publish -p:vm=" + buildfile["VM"].ToLower() +" -r win-x64 -c debug MOOS -p:EmitLegacyAssetsFileItems=true").WaitForExit();
-            Environment.CurrentDirectory = a;
-            Directory.Delete(Dirname,true);
+
+            Directory.Delete("Corlib", true);
+            Directory.Delete("Kernel", true);
+            Directory.Delete("MOOS", true);
+            Directory.Delete("Ramdisk", true);
+
         }
         static void CopyDirectory(string sourceDir, string destDir,string ignore="")
         {
